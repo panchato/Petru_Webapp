b4065c7406425ea3624fb770edae326c1b52bec6
panchato <panchato@gmail.com>
2024-04-29 22:18:08 -0400

    Migrate to Postgresql

--- app/config.py
+++ app/config.py
@@ -6,8 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'very_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'instance', 'database.db')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://doadmin:AVNS_-iK8H6-2D_M_AJ7aSWn@db-postgresql-sfo3-petru-webapp-do-user-3433219-0.c.db.ondigitalocean.com:25060/petru-database?sslmode=require'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_COOKIE_HTTPONLY = True
     PERMANENT_SESSION_LIFETIME = timedelta(hours=12)
