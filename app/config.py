9c0fdba7b12a784887dc6a840ed5825200c37f9e
panchato <panchato@gmail.com>
2024-05-03 14:39:58 -0400

    DEBUG false

--- app/config.py
+++ app/config.py
@@ -1,15 +1,23 @@
 import os
 from datetime import timedelta
-from re import DEBUG
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = '}8d[26ER?q0hB#C?2pg2AXqnimQ9zKzqjzw($0*;M,Y#4,&h:1:j}2:'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://doadmin:AVNS_-iK8H6-2D_M_AJ7aSWn@db-postgresql-sfo3-petru-webapp-do-user-3433219-0.c.db.ondigitalocean.com:25060/petru-database?sslmode=require'
+    # Secret key for signing cookies
+    SECRET_KEY = os.getenv('SECRET_KEY')
+
+    # Database URI
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Session settings
     SESSION_COOKIE_HTTPONLY = True
     PERMANENT_SESSION_LIFETIME = timedelta(hours=12)
+
+    # File upload paths
     UPLOAD_PATH_IMAGE = os.path.join(basedir, 'static', 'images')
     UPLOAD_PATH_PDF = os.path.join(basedir, 'static', 'pdf')
+
+    # Allowed extensions for uploads
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'pdf'}
