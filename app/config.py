1ab8cd6165d0b81740bf90733e6c93b340759efe
panchato <panchato@gmail.com>
2024-05-04 00:21:58 -0400

    Do Spaces config

--- app/config.py
+++ app/config.py
@@ -19,5 +19,12 @@ class Config(object):
     UPLOAD_PATH_IMAGE = os.path.join(basedir, 'static', 'images')
     UPLOAD_PATH_PDF = os.path.join(basedir, 'static', 'pdf')
 
+    DO_SPACES_REGION = os.getenv('DO_SPACES_REGION')
+    DO_SPACES_BUCKET = os.getenv('DO_SPACES_BUCKET')
+    DO_SPACES_ENDPOINT_URL = os.getenv('DO_SPACES_ENDPOINT_URL')
+    DO_SPACES_CDN_BASE_URL = os.getenv('DO_SPACES_CDN_BASE_URL')
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
     # Allowed extensions for uploads
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'pdf'}
