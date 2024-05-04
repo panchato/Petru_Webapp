4f28775b1bb0e98952ea0a282f68aa9c9211a293
panchato <panchato@gmail.com>
2024-05-04 00:22:15 -0400

    Added load_dotenv

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object(Config)
